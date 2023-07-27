ec1182eb59bbe995b9ee2d68c9c479a7bc20964b
Djacson Stvil <djacsonstvil@gmail.com>
2023-07-27 18:17:13 -0400

    changes made

--- problems/06-dynamic-interval-count.js
+++ problems/06-dynamic-interval-count.js
@@ -1,13 +1,13 @@
 /***********************************************************************
 Write a function, `dynamicIntervalCount`, that accepts a callback, a delay
-in milliseconds, and an optional amount as arguments. The function should 
-set an interval with the given callback and delay. If an amount argument 
-is passed, the interval should be cleared after the callback has been 
+in milliseconds, and an optional amount as arguments. The function should
+set an interval with the given callback and delay. If an amount argument
+is passed, the interval should be cleared after the callback has been
 called 'amount' number of times. If an amount argument is not passed,
 the interval should not be cleared and `dynamicIntervalCount` should instead
 return the Timeout object for the interval.
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
 
 Examples:
@@ -23,11 +23,40 @@ const timeoutObject = dynamicIntervalCount(function() {
 
 console.log(timeoutObject); // Timeout { ... }
 ***********************************************************************/
-
-function dynamicIntervalCount(cb, delay, amount) {
-  // Your code here
+function dynamicIntervalCount(cb, delay, ...amount){
+  let timer = setInterval(()=>{
+    cb()
+    if (amount.length !== 0){
+      amount[0]--
+      if (amount[0] === 0) clearInterval(timer)
+    }
+  }, delay)
+  return timer
 }
+/* function dynamicIntervalCount(cb, delay, amount) {
+  if (amount === undefined) return setInterval(cb, delay)
+  let timer = setInterval(()=>{
+    //debugger
+    cb()
+    amount--
+    if (amount.toString().length !== 0) {
+      if (amount === 0) clearInterval(timer)
+    }
 
+  }, delay)
+  return timer
+} */
+/*
+dynamicIntervalCount(function() {
+  console.log('hi');
+}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
+ */
+
+const timeoutObject = dynamicIntervalCount(function() {
+  console.log('hi');
+}, 500); // prints 'hi' at 500ms intervals indefinitely
+
+console.log(timeoutObject); // Timeout { ... }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = dynamicIntervalCount;
